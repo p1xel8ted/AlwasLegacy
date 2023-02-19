b07f2638b742d31e711c4373c511528c853a9f64
p1xel8ted <10510767+p1xel8ted@users.noreply.github.com>
2023-02-19 16:43:07 +1100

    Initial
    Initial

---
+++ AlwasLegacy/Plugin.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace AlwasLegacy;
+
+[BepInPlugin("p1xel8ted.alwaslegacy.fixes", "Alwa's Legacy Fixes", "0.1.1")]
+[HarmonyPatch]
+public partial class Plugin : BaseUnityPlugin
+{
+    private static Harmony _hi;
+    private static ManualLogSource _log;
+    private static ConfigEntry<int> _horizontalScrollAdjustment = null!;
+    private static ConfigEntry<float> _dialogScale = null!;
+    
+    public void Awake()
+    {
+        _horizontalScrollAdjustment = Config.Bind("General", "HorizontalScrollAdjustment", 300, "Change when the game initiates a scroll. The current setting will scroll right when player is within 300 pixels of the left/right edge of the screen. This setting allows you to adjust that value.");
+        _dialogScale = Config.Bind("General", "DialogScale", 0.75f, "Scale of the NPC dialogues and item pickup dialogues. 0.75 is the default (makes it a littler smaller).");
+        
+        _hi = Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly());
+        _log = new ManualLogSource("Log");
+        BepInEx.Logging.Logger.Sources.Add(_log);
+        _log.LogInfo($"Plugin Alwa's Legacy Fixes is loaded!");
+    }
+
+    public void OnDestroy()
+    {
+        _hi?.UnpatchSelf();
+    }
+    
+
+}
---
+++ AlwasLegacy/UnityEvents.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using HarmonyLib;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace AlwasLegacy;
+
+[HarmonyPatch]
+public partial class Plugin
+{
+    public void LateUpdate()
+    {
+        var dialogUI = GameObject.Find("Canvas/MoveCanvasAtStart/DialogGUI");
+        if (dialogUI != null)
+        {
+            dialogUI.transform.localScale = new Vector3(_dialogScale.Value, _dialogScale.Value, _dialogScale.Value);
+        }
+
+        var itemPickup = GameObject.Find("Canvas/MoveCanvasAtStart/ItemPickup");
+        if (itemPickup != null)
+        {
+            itemPickup.transform.localScale = new Vector3(_dialogScale.Value, _dialogScale.Value, _dialogScale.Value);
+        }
+
+        if (Camera.current != null)
+        {
+            var cameraScript = Camera.current.gameObject.GetComponentInChildren<CameraScript>();
+            if (cameraScript != null)
+            {
+                //  _log.LogWarning("Found CameraScript!");
+                var res = SettingsManager.instance.resolutionTexts[SystemSaveSettings.instance.resolutionIndex];
+                var widthStr = res.Split('x')[0];
+                var widthInt = int.Parse(widthStr);
+                if (Math.Abs(cameraScript.playerScreenPos.x - _horizontalScrollAdjustment.Value) < 25f)
+                {
+                    cameraScript.playerScreenPos.x = 0;
+                }
+
+                if (Math.Abs(cameraScript.playerScreenPos.x - (widthInt - _horizontalScrollAdjustment.Value)) < 25f)
+                {
+                    cameraScript.playerScreenPos.x = widthInt;
+                }
+            }
+        }
+
+        var stuff3 = GameObject.Find("Canvas/MoveCanvasAtStart/CutsceneBorders");
+        if (stuff3 != null)
+        {
+            stuff3.SetActive(false);
+        }
+
+        var stuff4 = GameObject.Find("Canvas/MoveCanvasAtStart/InfoPopup");
+        if (stuff4 != null)
+        {
+            if (Math.Abs(stuff4.transform.position.x - 1720) < 0.001)
+            {
+                var position = stuff4.transform.position;
+                position = new Vector3(1620, position.y, position.z);
+                stuff4.transform.position = position;
+            }
+        }
+
+        var cutsceneBorders = FindObjectsOfType<CutsceneBorders>();
+        foreach (var cutsceneBorder in cutsceneBorders)
+        {
+            cutsceneBorder.enabled = false;
+        }
+
+        if (!SceneManager.GetActiveScene().name.Contains("Main"))
+        {
+            if (Camera.current != null) Camera.current.backgroundColor = Color.black;
+        }
+
+        var canvasScalers = FindObjectsOfType<CanvasScaler>();
+        foreach (var canvasScaler in canvasScalers)
+        {
+            canvasScaler.referenceResolution = new Vector2(640, 350);
+            canvasScaler.screenMatchMode = CanvasScaler.ScreenMatchMode.Expand;
+            canvasScaler.uiScaleMode = CanvasScaler.ScaleMode.ScaleWithScreenSize;
+        }
+    }
+}
